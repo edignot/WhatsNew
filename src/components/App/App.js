52d166f3f2fb6ec54400f167ca1ba22c474d8a92
Ed Ignot <edignot@gmail.com>
2020-05-04 17:51:39 -0600

    Importing datat files to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import local from '../../data/local';
 import './App.css';
 import SearchForm from '../SearchForm/SearchForm'
 import Menu from '../Menu/Menu'
 import NewsContainer from '../NewsContainer/NewsContainer'
+import local from '../../data/local';
+import entertainment from '../../data/entertainment.js'
+import health from '../../data/health.js'
+import science from '../../data/science.js'
+import technology from '../../data/technology.js'
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      news: [local, entertainment, health, science, technology]
     }
   }
 
   render () {
+    console.log(this.state.news)
     return (
       <div className="app">
         <SearchForm />
