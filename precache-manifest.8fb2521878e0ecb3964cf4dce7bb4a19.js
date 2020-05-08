7fdb19b8c4f8232393b65cb250ae862746be0b3e
Ed Ignot <edignot@gmail.com>
2020-05-07 20:03:39 -0600

    Updates

---
+++ precache-manifest.8fb2521878e0ecb3964cf4dce7bb4a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d5e22d51cb68bd2746580fd627f1a3",
+    "url": "/WhatsNew/index.html"
+  },
+  {
+    "revision": "70aa8acdec808c1c722f",
+    "url": "/WhatsNew/static/css/main.817e7e44.chunk.css"
+  },
+  {
+    "revision": "9cb6184a6fce5e608afe",
+    "url": "/WhatsNew/static/js/2.2a8b2e1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WhatsNew/static/js/2.2a8b2e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70aa8acdec808c1c722f",
+    "url": "/WhatsNew/static/js/main.1b71f6a0.chunk.js"
+  },
+  {
+    "revision": "ff79ba843500aa2a75d0",
+    "url": "/WhatsNew/static/js/runtime-main.bafe8e32.js"
+  }
+]);
